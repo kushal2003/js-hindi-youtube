dad8cbc5e4bd8b01e9f55a7e29b1b8fca8f7127c
kushal2003 <61346809+kushal2003@users.noreply.github.com>
2023-10-07 11:50:54 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "kushal@gmail.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+let accountState ;
+
+// accountId =2
+console.log(accountId); 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+// we prefer not to use var because of the issue in the block and the functional scope
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) node ke andar yeh error dega , developer console mei work krega yehh 
+console.log(3+3)
+console.log("hitesh")
+
+let name ="kushal"
+let age=69
+let isloggedin=true 
+let state =null 
+
+//PRIMITIVE
+
+//number , 2 ^ 53 range 
+//bigint 
+// string - single or double quotes
+
+//boolean - true or false 
+// null => this is a standalone value , empty value 
+//undefined => matlab value hee assign nahi hui hai 
+
+//symbol =>unique
+
+//OBJECTS
+
+console.log(typeof (null)) //iska type is OBJECT
+console.log(typeof undefined ) // iska type is UNDEFINED
+
