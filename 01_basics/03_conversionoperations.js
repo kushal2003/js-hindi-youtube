437ddd1db13b6e5f23519494d4c1deb67e6b3bca
kushal2003 <61346809+kushal2003@users.noreply.github.com>
2023-10-08 03:13:32 +0000

    Conversions

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,39 @@
+let score =33 ;
+let abc="44";
+let def="33abc";
+let pqr=null;
+// console.log(typeof score);//method1 
+// console.log(typeof(score))
+
+// let valueInABC=Number(abc);
+// console.log(typeof valueInABC);
+// let valueinDEF=Number(def);
+// console.log(typeof valueinDEF);
+
+// console.log(valueinDEF);
+// console.log(pqr)
+
+//"33" => 33
+//"33abc" =>    NaN
+//true => 1.   ; false => 0 
+
+let isLoggedIn1 = 1;
+let isLoggedIn2 = "";
+let isLoggedIn3 = "a";
+let isLoggedIn4 = 5;
+let isLoggedIn5 = -5;
+let isLoggedIn6 = 0 ;
+
+
+let BooleanIsLoggedIn1=Boolean(isLoggedIn6);
+// console.log(BooleanIsLoggedIn1);
+
+
+
+//"" and 0 pe falsee aayega boolean value 
+
+//Now to convert to string => we'll use String
+
+let StringIsLoggedIn=String(isLoggedIn1);
+console.log(StringIsLoggedIn);
+console.log(typeof (StringIsLoggedIn));
