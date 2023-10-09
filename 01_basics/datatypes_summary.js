8f34c6f9395970b14b33255ad2ebf64adf449377
kushal2003 <61346809+kushal2003@users.noreply.github.com>
2023-10-09 00:53:23 +0000

    stack heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -23,5 +23,31 @@ let myObj={
 const myFunction= function(){
     console.log("Hello World")
 }
-console.log(typeof heros)
-console.log(typeof id)
+// console.log(typeof heros)
+// console.log(typeof id)
+
+
+//MEMORIES
+
+// Types : Stack(Primitive) , Heap (Non - Primitve)
+// Stack-we  get a copy , Heap- reference milta hai toh changes original wale mei bhi reflect hote hai 
+
+let a="kushal mittal"
+let b=a; // yaha b ko ek copy mili hai original nhi mila , toh orignal mei changes show nhi honge when they are done in b
+b="sigma"
+console.log(a)
+console.log(b)
+
+
+// heap 
+let myObject=
+{
+    name:"abc",
+    age:69
+}
+let obj2=myObject;
+obj2.name="pqr";
+console.log(myObject)
+console.log(obj2)
+
+
