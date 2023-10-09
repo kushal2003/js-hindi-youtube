33e548976d39fd54da2ba49e7fd8b7b621393e89
kushal2003 <61346809+kushal2003@users.noreply.github.com>
2023-10-09 02:22:10 +0000

    STRINGS

---
+++ 01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name="abc";
+const repoCount=50;
+console.log(name + repoCount + " value")
+console.log(`hello ${name} and the repoCount is ${repoCount}`) //STRING INTERPOLATION 
+
+const gameName=new String('The Kushal Mittal')
+const g2=new String();
+console.log(g2)
+
+console.log(gameName.length)
+console.log(gameName.indexOf('t'))
+
+const url= "https://abc.com"
+console.log(url.replace('com','in'))
+console.log(url.includes('abc'))
