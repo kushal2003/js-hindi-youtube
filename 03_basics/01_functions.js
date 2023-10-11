64a051aa84753ce1fa201f8509444131d22117bd
kushal2003 <61346809+kushal2003@users.noreply.github.com>
2023-10-11 01:26:01 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,42 @@
+function sayMyName(){
+    console.log("Hello")
+}
+
+//execution 
+sayMyName()
+
+
+function addTwoNumbers(number1 , number2){
+    console.log(number1+number2);
+}
+// addTwoNumbers(3,7)
+
+
+function loginUserMessage(username){
+    if (username === undefined)//or if(!username)
+    {
+        console.log('please enter the username')
+        return ;
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Kushal"))
+
+function calculateCartPrice(num1){
+    return num1;
+}
+//REST FUNCTION    ... WALA 
+
+function calculateCartPrice(val1,val2,...num1)
+{
+    return num1;
+}
+let result =calculateCartPrice(100,200,300,499);
+// console.log(result);
+
+
+function handleObject(anyObject)
+{
+    console.log(`username is ${anyObject} and the price is ${anyObject.price}`)
+}
